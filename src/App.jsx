79639b603dde7cd632c876122d92f824be48ca94
Kyaw San Oo <skyaw6736@gmail.com>
2024-12-16 17:48:20 +0630

    create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
