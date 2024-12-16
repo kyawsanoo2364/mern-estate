c20b3b9d5714e02a500dfd73bac5be18d7806df2
Kyaw San Oo <skyaw6736@gmail.com>
2024-12-16 19:38:06 +0630

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to database..."))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="text-sm sm:text-xl flex flex-wrap font-bold">
-            <span className="text-slate-400">Sahand</span>
+            <span className="text-slate-400">K.S.O</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
